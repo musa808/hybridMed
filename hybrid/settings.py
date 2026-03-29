17bdad3e5e45686a09b19f6099e84f00769acedf
musa808 <mohamudmusa115@gmail.com>
2026-03-29 23:07:29 +0300

    fixed

--- hybrid/settings.py
+++ hybrid/settings.py
@@ -49,10 +49,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 DEBUG = True
 USE_I18N = True
@@ -75,7 +74,7 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Application definition
 
 INSTALLED_APPS = [
